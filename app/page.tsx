e6e8346be367e69c3f5e796b702bf7ba5bbb7e1b
dawitzeleke <dawitzeleke92@gmail.com>
2026-05-22 21:30:55 +0300

    Set up the shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <div>
-    <h1>Hello, World!</h1>
-  </div>;
+  return (
+    <div className="p-10 bg-shop-light-pink">
+      <h1>Hello, World!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt
+        distinctio sapiente fugit autem non ipsa odit necessitatibus ipsam
+        aliquid itaque ipsum, deserunt sit aperiam. Quae, consequuntur
+        excepturi? Sequi, neque cum.
+      </p>
+
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default Home;
