7743c789cb8e44266ac9720902350449debdce11
dawitzeleke <dawitzeleke92@gmail.com>
2026-05-26 10:20:41 +0300

    Finish the navbar menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,21 @@
 import Container  from './Container'
 import React from 'react'
 import Logo from './Logo'
+import HeaderMenu from './HeaderMenu'
+import SearchBar from './SearchBar'
 
 const Header = () => {
   return (
-    <header className='bg-white py-5'>
-        <Container>
+    <header className='bg-white py-5 border-b border-b-black/20'>
+        <Container className='flex items-center justify-between'>
             {/* Logo */}
             <Logo/>
             
+            <HeaderMenu/>
+
+            <div>
+              <SearchBar/>
+            </div>
             {/* NavButton */}
 
             {/* Navicon */}
---
+++ components/HeaderMenu.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { headerDate } from "@/constants/data";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const HeaderMenu = () => {
+const pathName = usePathname();
+console.log(pathName);
+  return (
+    <div className="hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor">
+      {headerDate?.map((item) => (
+        <Link
+          key={item?.title}
+          href={item.href}
+          className={`hover:text-shop_light_green hoverEffect relative group ${pathName === item?.href ? 'text-shop_light_green' : ''}`}
+        >
+          {item?.title}
+
+          <span
+            className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:left-0 ${pathName === item?.href && 'w-1/2'}`}
+          />
+          <span
+            className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:right-0  ${pathName === item?.href && 'w-1/2'}`}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default HeaderMenu;
